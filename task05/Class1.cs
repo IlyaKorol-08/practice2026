cae7ae9df83001fa118d6431e2557afaea7a488c
Ilya Korol <iliakorol2008@gmail.com>
2026-06-26 19:03:21 +0600

    Добавлен task05

---
+++ task05/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods()
+    {
+        return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                    .Select(m => m.Name)
+                    .Distinct();
+    }
+
+    public IEnumerable<string> GetMethodParams(string methodName)
+    {
+        var method = _type.GetMethod(methodName, 
+            BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+        
+        if (method == null)
+        {
+            return Enumerable.Empty<string>();
+        }
+        var parameters = method.GetParameters()
+                               .Select(p => $"{p.ParameterType.Name} {p.Name}");
+        
+        var returnType = $"Return: {method.ReturnType.Name}";
+
+        return parameters.Append(returnType);
+    }
+
+    public IEnumerable<string> GetAllFields()
+    {
+        return _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | 
+                               BindingFlags.Instance | BindingFlags.Static)
+                    .Select(f => f.Name);
+    }
+
+    public IEnumerable<string> GetProperties()
+    {
+        return _type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | 
+                                   BindingFlags.Instance | BindingFlags.Static)
+                    .Select(p => p.Name);
+    }
+
+    public bool HasAttribute<T>() where T : Attribute
+    {
+        return _type.GetCustomAttributes(typeof(T), true).Any();
+    }
+}
---
+++ task05tests/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿namespace task05tests
+{
+    public class TestClass
+    {
+        public int PublicField;
+        private string _privateField;
+        public int Property { get; set; }
+
+        public void Method() { }
+    }
+
+    [Serializable]
+    public class AttributedClass { }
+
+    public class ClassAnalyzerTests
+    {
+        [Fact]
+        public void GetPublicMethods_ReturnsCorrectMethods()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var methods = analyzer.GetPublicMethods();
+
+            Assert.Contains("Method", methods);
+        }
+
+        [Fact]
+        public void GetAllFields_IncludesPrivateFields()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var fields = analyzer.GetAllFields();
+
+            Assert.Contains("_privateField", fields);
+        }
+
+        [Fact]
+        public void GetProperties_ReturnsPropertyNames()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+            var properties = analyzer.GetProperties();
+
+            Assert.Contains("Property", properties);
+        }
+
+        [Fact]
+        public void HasAttribute_WithAttribute_ReturnsTrue()
+        {
+            var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+
+            Assert.True(analyzer.HasAttribute<SerializableAttribute>());
+        }
+
+        [Fact]
+        public void HasAttribute_WithoutAttribute_ReturnsFalse()
+        {
+            var analyzer = new ClassAnalyzer(typeof(TestClass));
+
+            Assert.False(analyzer.HasAttribute<SerializableAttribute>());
+        }
+    }
+}
