b120bff2a337aa1b8bf4ede394b1ec3bb9cb94a8
IlyaKorol-08 <iliakorol2008@gmail.com>
2026-07-16 00:10:28 +0600

    Добавлен task17

---
+++ task17/Class1.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Concurrent;
+
+namespace task17;
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public class ServerThread
+{
+    private readonly BlockingCollection<ICommand> _queue = new();
+    private readonly Thread _thread;
+    private volatile bool _hardStop = false;
+    private volatile bool _softStop = false;
+
+    public ServerThread()
+    {
+        _thread = new Thread(Run)
+        {
+            IsBackground = true
+        };
+    }
+
+    public void Start()
+    {
+        _thread.Start();
+    }
+
+    public Thread GetThread()
+    {
+        return _thread;
+    }
+
+    public void Enqueue(ICommand command)
+    {
+        _queue.Add(command);
+    }
+
+    public void HardStop()
+    {
+        _hardStop = true;
+    }
+
+    public void SoftStop()
+    {
+        _softStop = true;
+    }
+
+    private void Run()
+    {
+        while (!_hardStop)
+        {
+            if (_softStop && _queue.Count == 0)
+                break;
+
+            try
+            {
+                if (_queue.TryTake(out var command, TimeSpan.FromMilliseconds(100)))
+                {
+                    command.Execute();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in command: {ex.Message}");
+            }
+        }
+    }
+}
+
+public class HardStopCommand : ICommand
+{
+    private readonly ServerThread _targetThread;
+
+    public HardStopCommand(ServerThread targetThread)
+    {
+        _targetThread = targetThread;
+    }
+
+    public void Execute()
+    {
+        if (Thread.CurrentThread != _targetThread.GetThread())
+            throw new InvalidOperationException("HardStop должен выполняться в том же потоке, который останавливает");
+
+        _targetThread.HardStop();
+    }
+}
+
+public class SoftStopCommand : ICommand
+{
+    private readonly ServerThread _targetThread;
+
+    public SoftStopCommand(ServerThread targetThread)
+    {
+        _targetThread = targetThread;
+    }
+
+    public void Execute()
+    {
+        if (Thread.CurrentThread != _targetThread.GetThread())
+            throw new InvalidOperationException("SoftStop должен выполняться в том же потоке, который останавливает");
+
+        _targetThread.SoftStop();
+    }
+}
+
+public class TestCommand : ICommand
+{
+    private readonly Action _action;
+
+    public TestCommand(Action action)
+    {
+        _action = action;
+    }
+
+    public void Execute()
+    {
+        _action();
+    }
+}
---
+++ task17tests/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using task17;
+
+namespace task17tests;
+
+public class ServerThreadTests
+{
+    [Fact]
+    public void HardStop_StopsThreadImmediately()
+    {
+        var server = new ServerThread();
+        var commandsExecuted = new List<int>();
+        
+        server.Enqueue(new TestCommand(() => commandsExecuted.Add(1)));
+        server.Enqueue(new HardStopCommand(server));
+        server.Enqueue(new TestCommand(() => commandsExecuted.Add(2)));
+        
+        server.Start();
+        server.GetThread().Join(5000);
+        
+        Assert.Single(commandsExecuted);
+        Assert.Equal(1, commandsExecuted[0]);
+    }
+
+    [Fact]
+    public void SoftStop_ExecutesAllQueuedCommands()
+    {
+        var server = new ServerThread();
+        var commandsExecuted = new List<int>();
+        
+        server.Enqueue(new TestCommand(() => commandsExecuted.Add(1)));
+        server.Enqueue(new TestCommand(() => commandsExecuted.Add(2)));
+        server.Enqueue(new SoftStopCommand(server));
+        server.Enqueue(new TestCommand(() => commandsExecuted.Add(3)));
+        
+        server.Start();
+        server.GetThread().Join(5000);
+        
+        Assert.Equal(3, commandsExecuted.Count);
+        Assert.Contains(1, commandsExecuted);
+        Assert.Contains(2, commandsExecuted);
+        Assert.Contains(3, commandsExecuted);
+    }
+
+    [Fact]
+    public void HardStop_FromAnotherThread_ThrowsException()
+    {
+        var server = new ServerThread();
+        var hardStop = new HardStopCommand(server);
+        
+        Assert.Throws<InvalidOperationException>(() => hardStop.Execute());
+    }
+
+    [Fact]
+    public void SoftStop_FromAnotherThread_ThrowsException()
+    {
+        var server = new ServerThread();
+        var softStop = new SoftStopCommand(server);
+        
+        Assert.Throws<InvalidOperationException>(() => softStop.Execute());
+    }
+
+    [Fact]
+    public void ServerThread_HandlesException_ContinuesProcessing()
+    {
+        var server = new ServerThread();
+        var commandsExecuted = new List<int>();
+        
+        server.Enqueue(new TestCommand(() => throw new Exception("Test exception")));
+        server.Enqueue(new TestCommand(() => commandsExecuted.Add(1)));
+        server.Enqueue(new SoftStopCommand(server));
+        
+        server.Start();
+        server.GetThread().Join(5000);
+        
+        Assert.Single(commandsExecuted);
+        Assert.Equal(1, commandsExecuted[0]);
+    }
+}
